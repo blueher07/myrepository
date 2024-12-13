a70af3ab435fc18a161309e6c0890dbf29fad57f
blueher07 <43721359+blueher07@users.noreply.github.com>
2024-12-14 00:03:44 +0200

    Add files via upload

---
+++ 10 euros and 90 cents.py
@@ -0,0 +1,14 @@
+#3 ways of printing  phrase "I have 10 euros and 90 cents"
+ 
+euros=10
+cents=90
+print ("I have %s euros and %s cents" %(euros, cents))
+
+
+euros = 10
+cents = 90
+print("I have", euros, "euros and", cents, "cents")
+
+euros = 10
+cents = 90
+print(f"I have {euros} euros and  {cents} cents")
